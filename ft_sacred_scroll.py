bd55f720fc8c0406e154260114f2810d4d893a41
Abdellatif Nahal <abdenah17@gmail.com>
2026-03-06 04:18:57 +0000

    part 1

---
+++ ft_sacred_scroll.py
@@ -0,0 +1,9 @@
+import alchemy
+import alchemy.elements
+
+print("=== Sacred Scroll Mastery ===\n")
+print("Testing direct module access:")
+alchemy.elements.create_fire()
+alchemy.elements.create_water()
+alchemy.elements.create_earth()
+alchemy.elements.create_air()
